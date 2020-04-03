b1bccc42796c2716ca80e989483765ec8c9d9629
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-04-03 13:29:40 +0200

    Update Application.java

--- src/be/intecbrussel/App/Application.java
+++ src/be/intecbrussel/App/Application.java
@@ -1,50 +1,32 @@
 package be.intecbrussel.App;
 
-import be.intecbrussel.eatbles.*;
+import be.intecbrussel.eatbles.Cone;
+import be.intecbrussel.eatbles.Eatable;
+import be.intecbrussel.eatbles.IceRocket;
+import be.intecbrussel.eatbles.Magnum;
 import be.intecbrussel.seller.IceCreamSalon;
+import be.intecbrussel.seller.IceCreamSeller;
 import be.intecbrussel.seller.PriceList;
 
-public class Application {
-    public static void main(String[] args) {
-
-        PriceList price = new PriceList(2.0,3.50,4.20);
-        price.arii();
-        IceCreamSalon salon = new IceCreamSalon(new PriceList(2,5,5));
-        Magnum m = new Magnum();
-        m.eat();
-        Cone c = new Cone();
-        c.eat();
+public class Apppp {
+    public static void main (String [] args){
 
 
-        Eatable[ ] ice;
-        ice = new Eatable[]{
-                new Magnum(Magnum.MagnumType.BLACKCHOCOLATE),
-                new Magnum(Magnum.MagnumType.ALPINENUTS),
-                new Magnum(Magnum.MagnumType.MILKCHOCOLATE),
-                new Magnum(Magnum.MagnumType.ROMANTICSTRAWBERRI),
-                new Magnum(Magnum.MagnumType.WHITECHOCOLATE),
-                new Cone(new Cone.Flavor[]{Cone.Flavor.VANILLA, Cone.Flavor.CHOCOLATE,
-                Cone.Flavor.STRACIATELLA, Cone.Flavor.LEMON , Cone.Flavor.BANANA, Cone.Flavor.STRAWBERRY}),
-                new IceRocket()};
+        IceCreamSeller icesalon = new IceCreamSalon(new PriceList(9.10,6.20,7));
 
-     //   System.out.println(Arrays.toString(ice));
-        Eatable [] iceCream = new Eatable[]{
-                new Magnum(Magnum.MagnumType.BLACKCHOCOLATE),
-                new Magnum(Magnum.MagnumType.ALPINENUTS),
-           new Cone(new Cone.Flavor[] {Cone.Flavor.STRAWBERRY , Cone.Flavor.LEMON}) , new IceRocket()};
+        icesalon.orderIceRocket();
+        icesalon.orderMagnum(Magnum.MagnumType.ROMANTICSTRAWBERRI);
+       // icesalon.orderCone();
 
-
-            for(Eatable orders : iceCream){
-                if(orders instanceof Cone) {
-                    salon.orderCone(((Cone) orders).balls);}
-                    else if (orders instanceof Magnum){
-                    salon.orderMagnum(((Magnum) orders).getType());
-                }else if (orders instanceof IceRocket){
-                    salon.orderIceRocket();
-                    orders.eat();
-            }
-        }
-        //System.out.println(" youe order ga ot be " + Arrays.toString(iceCream));
+        Eatable icecreamI = new Magnum(Magnum.MagnumType.WHITECHOCOLATE);
+        Eatable icecreamIV = new Magnum(Magnum.MagnumType.ALPINENUTS);
+        Eatable icecreamII = new IceRocket();
+        Eatable icecreamIII = new Cone(new Cone.Flavor[]{Cone.Flavor.LEMON});
+        Eatable [] ice = {icecreamI , icecreamII , icecreamIII , icecreamIV};
+        ice[0].eat();
+        ice[1].eat();
+        ice[2].eat();
+        ice[3].eat();
+        System.out.println(" Kassa         " + icesalon.toString());
     }
-
 }
